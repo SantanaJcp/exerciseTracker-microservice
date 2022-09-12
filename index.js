c7ba9dcbaaa757756c087834dcce0cac31e2e90c
Jean Carlos Perez Santana <jean.perez@lever.co>
2022-09-11 23:28:37 -0400

    - initial structure added - database connection added

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan')
+const connectDB = require('./db/connect')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(cors());
+app.use(morgan('tiny'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server listening on port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+start()
